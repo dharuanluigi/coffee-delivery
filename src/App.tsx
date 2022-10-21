87ec34d03e7e147f8628f46b4a2c0def7a92dc05
Dharuan Luigi <dharuanluigi@gmail.com>
2022-10-20 21:53:35 -0300

    Added: base themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 /** @format */
 
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <div>Hello World</div> <h1>Hello</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+/** @format */
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body, input, text-area, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
